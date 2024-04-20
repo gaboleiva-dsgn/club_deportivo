ec574180ebba84c85ba1607fdbe2e6b429414220
Gabriel Leiva <gaboleiva@192.168.1.6>
2024-04-20 18:12:07 -0400

    Resuelto modificar

--- server.js
+++ server.js
@@ -4,48 +4,117 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
-app.listen(3000, () => console.log("Servidor escuchando Puerto: " + PORT));
-
+app.listen(3000, () => console.log("Servidor escuchando por Puerto: " + PORT));
+const _ = require("lodash");
 app.use(express.json());
 
 app.get("/agregar", (req, res) => {
 
     const { nombre, precio } = req.query;
 
-    if (!nombre || !precio) {
+    if (!nombre || !precio || isNaN(parseFloat(precio))) {
         return res.status(400).json({
             error: "Ups! se requiere nombre y precio"
         });
     }
 
-    const nuevoDeporte = {
-        nombre: nombre,
-        precio: precio
-    };
+    // Validamos si existe el archivo JSON deportes.json, si no lo creamos con un arreglo vacio
+    if (!fs.existsSync("deportes.json")) {
+        fs.writeFileSync("deportes.json", "[]", "utf8");
+    }
 
-    const { deportes } = JSON.parse(fs.readFileSync("deportes.json", "utf8")); deportes.push(nuevoDeporte);
+    // Leer el archivo JSON de deportes
+    let archivoDeportes = [];
 
-    fs.writeFileSync("deportes.json", JSON.stringify({ deportes }));
-    res.send('Deporte creado con exito');
+    try {
+        archivoDeportes = JSON.parse(fs.readFileSync("deportes.json", "utf8"));
+    } catch (error) {
+        console.error("Error al leer el archivo deportes.json: ", error.message);
+        return res.status(500).json({
+            error: "No se puede leer el archivo JSON desde el servidor",
+        });
+    }
+    console.log(`archivoDeportes es: ${archivoDeportes}`)
+    if (archivoDeportes.find((deporte) => deporte.nombre === nombre)) {
+        return res.status(400).json({
+            error: "Ya existe un deporte con ese nombre",
+        });
+    } else {
+        archivoDeportes.push({ nombre: nombre, precio: precio });
+        fs.writeFileSync("deportes.json", JSON.stringify({ deportes }));
+        res.send('Deporte creado con exito');
+    }
+
 });
 
 
 app.get("/deportes", (req, res) => {
-    const { deportes } = JSON.parse(fs.readFileSync("deportes.json", "utf8"));
+    // Verificar si el archivo JSON de deportes existe
+    if (!fs.existsSync("deportes.json")) {
+        // Si no existe, inicializar el archivo con un arreglo vacío
+        fs.writeFileSync("deportes.json", "[]", "utf8");
+    }
 
-    deportes.forEach(element => {
-        console.log(element.deporte + " " + element.precio);
+    // Leer el archivo JSON de deportes
+    let deportes = [];
+    try {
+        deportes = JSON.parse(fs.readFileSync("deportes.json", "utf8"));
+    } catch (error) {
+        console.error("Error al leer el archivo de deportes JSON: ", error.message);
+        return res.status(500).json({
+            error: "Error interno del servidor",
+        });
+    }
+    //usar lodash para ordenar el archivo json por la consola
+    deportes = _.orderBy(deportes, ["nombre"], ["asc"]);
+
+
+    // Enviar la lista de deportes en formato JSON como respuesta
+    console.log(deportes);
+    res.send({ deportes });
+});
+
+app.get("/modificar", (req, res) => {
+    const { nombre, precio } = req.query;
+
+    // Verificar el nombre y precio
+    if (!nombre || !precio) {
+        return res.send(
+            "Ingrese nombre del deporte para editar su valor, o todos los parámetros correspondientes"
+        );
+    }
+
+    // Verificar si el precio es un número
+    if (isNaN(parseFloat(precio))) {
+        return res.send("El precio debe ser un número válido");
+    }
+
+    // Leer el archivo de deportes.json
+    fs.readFile("deportes.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Error interno del servidor");
+        }
+        let deportes = JSON.parse(data);
+
+        // Busca el deporte y actualiza el valor
+        const index = deportes.findIndex((deporte) => deporte.nombre === nombre);
+        if (index === -1) {
+            return res.send("No existe ningún deporte con ese nombre");
+        }
+        deportes[index].precio = parseFloat(precio); // Convertir el precio a número
+        fs.writeFile("deportes.json", JSON.stringify(deportes), (err) => {
+            //guarda el valor
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Error interno del servidor");
+            }
+            res.send(`El precio del deporte ${nombre} se actualizó a ${precio}.`);
+        });
     });
-    res.send('Listado generado con exito!!!')
 });
 
-app.get("/editar/:nombre", (req, res) =>{
-    const deporte = req.params.nombre;
-    console.log(`El deporte que quiere editar es: ${deporte}`);
-    res.send("Todo ok");
-});
-
-app.get("/eliminar/:nombre", (req, res) =>{
+app.get("/eliminar/:nombre", (req, res) => {
     // Para recibir parametros debo enviar el parametro directo por la ruta ej: (http://localhost:3000/eliminar/Basketball)
     const nombreDeporte = req.params.nombre;
     //console.log(`El deporte que quiere eliminar es: ${deporte}`);
@@ -55,13 +124,13 @@ app.get("/eliminar/:nombre", (req, res) =>{
     const deportes = data.deportes;
     let busqueda = deportes.findIndex((deporte) => deporte.nombre == nombreDeporte);
 
-    if(busqueda == -1){
+    if (busqueda == -1) {
         console.log("El deporte: " + nombreDeporte + " no existe");
         return res.send("El deporte buscado no existe")
-        
+
     } else {
         console.log("El deporte es: ", deportes[busqueda]);
-        deportes.splice(busqueda,1);
+        deportes.splice(busqueda, 1);
         fs.writeFileSync("deportes.json", JSON.stringify(data));
     }
 
